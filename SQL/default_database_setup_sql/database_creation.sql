029bde44d39f2e723abe9155dd3f87ed786f4a4f
Unknown <barnwellrd@gmail.com>
2018-07-07 17:45:10 -0700

    Added database instructions
    The following instructions are used to create or reset the default database tables, data, and SPs

---
+++ SQL/default_database_setup_sql/database_creation.sql
@@ -0,0 +1,17 @@
+-- ******* DATABASE CONFIGURATION *******
+
+CONNECT SA AS SYSDBA;
+
+CREATE TABLESPACE new_tablespaceSpring2
+DATAFILE 'C:\[GITHUB_REPOSITORY_HOME_PATH]\lib\db\DATABASESpring2_FILE.dbf'
+SIZE 20M AUTOEXTEND ON;
+
+CREATE USER db_uSpring
+IDENTIFIED BY pass
+DEFAULT TABLESPACE new_tablespaceSpring2
+/
+
+GRANT DBA TO db_uSpring
+/
+
+CONNECT db_uSpring/pass
